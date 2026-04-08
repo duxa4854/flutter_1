8409553284c3686d81e5a65ba333e046cd867d7e
K1eranza <zubrevaevelina20641@gmail.com>
2026-04-08 14:01:30 +0300

    first commit

---
+++ bin/todo_app_v2.dart
@@ -0,0 +1,5 @@
+import 'package:todo_app_v2/todo_app_v2.dart' as todo_app_v2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${todo_app_v2.calculate()}!');
+}
---
+++ test/todo_app_v2_test.dart
@@ -0,0 +1,8 @@
+import 'package:todo_app_v2/todo_app_v2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
