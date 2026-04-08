420490deb4ac89adf753955073b126c4054a9cc7
K1eranza <zubrevaevelina20641@gmail.com>
2026-04-08 14:10:03 +0300

    inital

--- bin/todo_app_v2.dart
+++ bin/todo_app_v2.dart
@@ -1,5 +1,3 @@
-import 'package:todo_app_v2/todo_app_v2.dart' as todo_app_v2;
-
-void main(List<String> arguments) {
-  print('Hello world: ${todo_app_v2.calculate()}!');
+void main() {
+  print("Hello World!");
 }
--- test/todo_app_v2_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:todo_app_v2/todo_app_v2.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
